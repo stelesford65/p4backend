e1f917b0517c2ad04e4106a712886db7e2cdf629
Shawn  Telesford <shawn@MacBook-Pro.home>
2020-09-22 07:24:54 -0400

    Settings Adjusted

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -59,7 +59,11 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'google.com',
+    'https://stp4.netlify.app'
+)
+
 
 ROOT_URLCONF = 'p4backend.urls'
 
